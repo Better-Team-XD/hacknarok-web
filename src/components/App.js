122742ae3348f1b238aa6ceaba8322d4ee052954
krzysztof <misiek.nalepa@wp.pl>
2020-08-28 23:05:18 +0200

    init

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import HelloBlock from "./HelloBlock";
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <HelloBlock />
+            </div>
+        );
+    }
+}
+
+export default App;
