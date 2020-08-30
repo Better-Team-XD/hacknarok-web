ee004eccf20ac4bfdf4ee2dec5a0fa259a00266b
Krzysztof <zarnowski@student.agh.edu.pl>
2020-08-30 07:47:57 +0200

    landing page v3

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 class Footer extends React.Component {
     render() {
         return (
-            <div className={"page-footer font-small fixed-bottom"}>
+            <div className={"page-footer font-small"}>
                 <div className="footer-copyright text-center text-white py-3">
                     <a href={"https://github.com/Better-Team-XD"}>Created by Team XD</a>
                 </div>
